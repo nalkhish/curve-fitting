65a4bd028e9dd3fe9d3cbeb1f3897c012d0320c3
Nassir Al-Khishman <khishmann@gmail.com>
2022-11-29 21:08:08 -0500

    feat(plot) add plotter for tac

---
+++ src/pet_fit/plot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+from pet_fit.data import Tac
+
+
+def tac(tac: Tac, output_filename: str):
+    """Plot a TAC and save it to a file."""
+    fig, ax = plt.subplots()
+    plt.plot(tac.times, tac.concs)
+    plt.xlabel(tac.time_axis)
+    plt.ylabel(tac.conc_axis)
+    plt.title("Arterial Plasma")
+    fig.savefig(output_filename)
